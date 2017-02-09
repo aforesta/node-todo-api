cafa443ea922083bb61552def28c84619a5bb861
Antonio Foresta <antonio.foresta@atrivio.de>
2017-02-09 12:45:16 +0100

    Add POST /todos route and refactor mongoose

---
+++ server/models/user.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+var User = mongoose.model('User', {
+  // name: {
+  //   type: String,
+  //   required: true,
+  //   minlength: 1,
+  //   trim: true
+  // },
+  email: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  // },
+  // age: {
+  //   type: Number,
+  //   default: null
+  }
+});
+
+module.exports = {User};
